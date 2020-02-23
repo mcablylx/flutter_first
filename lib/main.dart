f9837f3d969243c53a55e17ddfd5df2eaa86bd69
马超 <308380504@qq.com>
2020-02-23 21:18:22 +0800

    init project

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('flutter demo'),
+        ),
+        body: HomeContent(),
+      ),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        "你好flutter 111",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 40.0,
+//          color: Colors.red
+          color: Color.fromRGBO(0, 0, 0, 2.0),
+        ),
+      ),
+    );
+  }
+}
